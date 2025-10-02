1d1c114abf9c8543b12c6ab2394ffc2dae8d6d53
Mykhail <its.astral.step@gmail.com>
2025-10-02 14:26:56 +0300

    added errors and success constants

--- fixtures/fixtures.ts
+++ fixtures/fixtures.ts
@@ -1,14 +1,18 @@
 import {test as base} from '@playwright/test'
 
 import {RegistrationPage} from "../pages/registration.page";
+import {LoginPage} from "../pages/login.page";
 
 export const test = base.extend<{
     registrationPage: RegistrationPage
+    loginPage: LoginPage
 }>({
     registrationPage: async({ page }, use) => {
         await use(new RegistrationPage(page));
     },
-
+    loginPage: async({ page }, use) => {
+        await use(new LoginPage(page));
+    }
 });
 
 export {expect, type Page, type Download, type Locator, type TestInfo} from '@playwright/test'
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,12 +2,13 @@ import { expect, test} from '../fixtures/fixtures'
 import {createRandomUser} from '../helpers/randomUser.helper'
 
 test.describe('Registration', () => {
-    test('Successful registration of a new user', async ({ registrationPage, page }) => {
+    test('Successful registration of a new user', async ({ registrationPage, loginPage, page }) => {
         await registrationPage.open()
         await registrationPage.newUserRegistration(createRandomUser())
         await registrationPage.submitButton.click()
         await expect(page).toHaveURL("https://www.redmine.org/login")
-
+        await expect(loginPage.successfulAccountCreationMessage).toBeVisible()
+        await expect(loginPage.successfulAccountCreationMessage).toHaveText("")
     })
 })
 
