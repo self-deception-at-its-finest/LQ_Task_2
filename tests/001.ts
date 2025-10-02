22444650dc76806558dcc7a22d4b9218588c230a
Mykhail <its.astral.step@gmail.com>
2025-10-02 14:01:29 +0300

    added base url to configs

---
+++ tests/001.ts
@@ -0,0 +1,17 @@
+import { expect, test} from '@playwright/test'
+import { RegistrationPage } from '../pages/registration.page'
+
+
+test.describe('Registration', () => {
+    test('Successful registration of a new user', async ({ page }) => {
+        const registrationPage = new RegistrationPage(page);
+        await registrationPage.open()
+
+
+
+
+
+
+
+    })
+})
