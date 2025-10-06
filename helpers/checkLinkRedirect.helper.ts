7183afa4b4e7d7226199a2f1f03fcaf2855bfcff
Mykhail <its.astral.step@gmail.com>
2025-10-06 13:33:47 +0300

    updated test 004

--- helpers/checkLinkRedirect.helper.ts
+++ helpers/checkLinkRedirect.helper.ts
@@ -3,7 +3,6 @@ import {expect, Locator, Page} from '@playwright/test'
 
 export async function checkLinkRedirect(linkLocator: string, expectedUrl: string, page: Page): Promise<void> {
     const elem = page.locator(linkLocator)
-    await expect(elem).toHaveAttribute('href', expectedUrl);
     await elem.click()
     await expect(page).toHaveURL(expectedUrl)
     await page.goBack()
